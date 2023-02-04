0cb2561e2110383606c951eb2e896431f9cbaf7b
harry <harry.papadopoulos@gugroup.com>
2023-02-04 17:45:09 +0200

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,192 @@
+// import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
+const {
+  getMessagesFromThread,
+  getGPTSummary,
+  getProjectIssueTypes,
+  getProjectLabels,
+  postTicket,
+  constructLabelOptionArray,
+  constructIssueTypeIdsArray,
+} = require("./utils");
+require("dotenv").config();
+
+const { App, LogLevel } = require("@slack/bolt");
+var provisionalTicket = {
+  labelsArray: null,
+  issueTypesArray: null,
+};
+
+// Initialize slack stuff
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+  // you still need to listen on some port!
+  port: process.env.PORT || 3000,
+  logLevel: LogLevel.INFO,
+});
+
+// Initialize openAI stuff
+const openai = new OpenAIApi(
+  new Configuration({ apiKey: process.env.OPENAI_API_KEY })
+);
+
+// Listen for messages and if a message mentions you respond with the modal button
+app.event("message", async ({ event, context }) => {
+  if (!event.thread_ts || !event.text.includes(`<@${context.botUserId}>`))
+    return;
+  console.log(event.thread_ts);
+  const slackMessagesString = await getMessagesFromThread(app, event, context);
+
+  provisionalTicket = await getGPTSummary(slackMessagesString, openai);
+
+  await app.client.chat.postMessage({
+    token: context.botToken,
+    channel: event.channel,
+    thread_ts: event.thread_ts,
+    text: "GPT response",
+    blocks: [
+      {
+        type: "actions",
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Open Modal",
+            },
+            value: "open_modal",
+            action_id: "open_modal_button", // TODO: Might wanna generate unique action IDs
+          },
+        ],
+      },
+    ],
+  });
+
+  const labels = await getProjectLabels();
+  const labelsArray = constructLabelOptionArray(labels);
+  const issueTypeIds = await getProjectIssueTypes();
+  const issueTypeIdsArr = constructIssueTypeIdsArray(issueTypeIds);
+  provisionalTicket["labelsArray"] = labelsArray;
+  provisionalTicket["issueTypesArray"] = issueTypeIdsArr;
+});
+
+// When the user opens the modal, send him the view
+app.action("open_modal_button", async ({ ack, body, context }) => {
+  ack();
+  try {
+    await app.client.views.open({
+      token: context.botToken,
+      trigger_id: body.trigger_id,
+      view: {
+        callback_id: "ticket_view",
+        title: {
+          type: "plain_text",
+          text: "Create new Jira issue",
+        },
+        submit: {
+          type: "plain_text",
+          text: "Create Jira issue",
+        },
+        blocks: [
+          {
+            block_id: "ticket_info",
+            type: "actions",
+            elements: [
+              {
+                type: "static_select",
+                placeholder: {
+                  type: "plain_text",
+                  text: "Select a conversation",
+                  emoji: true,
+                },
+                action_id: "label",
+                options: provisionalTicket.labelsArray,
+              },
+              {
+                type: "static_select",
+                placeholder: {
+                  type: "plain_text",
+                  text: "Select a channel",
+                  emoji: true,
+                },
+                action_id: "issueType",
+                options: provisionalTicket.issueTypesArray,
+              },
+            ],
+          },
+          {
+            block_id: "ticket_title",
+            type: "input",
+            element: {
+              type: "plain_text_input",
+              action_id: "ticket_title_input",
+              placeholder: {
+                type: "plain_text",
+                text: "Ex: Migrate to tailwind",
+              },
+              initial_value: provisionalTicket.title,
+            },
+            label: {
+              type: "plain_text",
+              text: "Title",
+            },
+            hint: {
+              type: "plain_text",
+              text: "The title of the Jira issue",
+            },
+          },
+          {
+            block_id: "ticket_description",
+            type: "input",
+            element: {
+              type: "plain_text_input",
+              action_id: "ticket_description_input",
+              multiline: true,
+              placeholder: {
+                type: "plain_text",
+                text: "Ex: MaterialUI bad, tailwind good",
+              },
+              initial_value: provisionalTicket.description,
+            },
+            label: {
+              type: "plain_text",
+              text: "Description",
+            },
+            hint: {
+              type: "plain_text",
+              text: "The description of the Jira issue",
+            },
+          },
+        ],
+        type: "modal",
+      },
+    });
+  } catch (error) {
+    logger.error("Coudln't open modal", error);
+  }
+});
+
+// Post the ticket to Jira
+app.view("ticket_view", async ({ payload, ack }) => {
+  await ack();
+  const label = payload.state.values.ticket_info.label.selected_option.value;
+  const issueType =
+    payload.state.values.ticket_info.issueType.selected_option.value;
+  postTicket(
+    payload.state.values.ticket_title.ticket_title_input.value,
+    payload.state.values.ticket_description.ticket_description_input.value,
+    label,
+    issueType
+  );
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log("GPTJira is running!");
+})();
---
+++ utils.js
@@ -0,0 +1,155 @@
+const getGPTSummary = async (conversation, openai) => {
+  const basePrompt =
+    'The following is a discussion between 2 people at a company. Create a Jira ticket with a "Title" and a "Description" section relative to what they are talking about:\n';
+  try {
+    const fullPrompt = basePrompt + conversation;
+
+    const completionResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: fullPrompt,
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const completionText = completionResponse.data.choices[0].text;
+
+    const titleRegex = /Title: (.*)\n/;
+    const descriptionRegex = /Description: (.*)/;
+
+    const title = completionText.match(titleRegex)[1];
+    const description = completionText.match(descriptionRegex)[1];
+    return { title, description };
+  } catch (error) {
+    // or error.response for gpt api failures
+    console.log("ERRROR", error);
+  }
+};
+
+const getMessagesFromThread = async (app, event, context) => {
+  const messagesInThreadResponse = await app.client.conversations.replies({
+    token: context.botToken,
+    channel: event.channel,
+    ts: event.thread_ts,
+  });
+
+  const strippedMessages = messagesInThreadResponse.messages.map(
+    (message) => `${message.user}:${message.text}/n `
+  );
+
+  const messageString = strippedMessages.join("");
+  return messageString;
+};
+
+const getProjectLabels = async () => {
+  try {
+    const res = await authFetch(
+      `${process.env.JIRA_PROJECT_URL}/rest/api/3/label`,
+      "GET"
+    );
+
+    // for some reason it only gives me the labels that I've used in a ticket
+    const responseLabel = await res.json();
+    return responseLabel.values;
+  } catch (e) {
+    console.log("couldnt get project labels", e);
+  }
+};
+
+const getProjectIssueTypes = async () => {
+  const projectId = process.env.JIRA_PROJECT_ID;
+  try {
+    const res = await authFetch(
+      `${process.env.JIRA_PROJECT_URL}/rest/api/3/issue/createmeta?projectIds=${projectId}`,
+      "GET"
+    );
+
+    const projectsObj = await res.json();
+
+    const issueTypes = projectsObj.projects[0].issuetypes;
+    const issueTypeIds = issueTypes.map((issueTypeObject) => {
+      return issueTypeObject.name;
+    });
+    return issueTypeIds;
+  } catch (e) {
+    console.log("Couldn't get issue types", e);
+  }
+};
+
+const authFetch = async (url, options) => {
+  const email = process.env.JIRA_ACCOUNT_EMAIL;
+  const token = process.env.JIRA_ACCOUNT_BOT_TOKEN;
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Basic ${Buffer.from(email + ":" + token).toString(
+      "base64"
+    )}`,
+  };
+  options = {
+    ...options,
+    headers: {
+      ...options?.headers,
+      ...headers,
+    },
+  };
+  return await fetch(url, options);
+};
+
+const getJiraTicketBody = (title, description, label, issueType) => {
+  const projectIssueTypes = getProjectIssueTypes();
+  const projectId = process.env.JIRA_PROJECT_ID;
+  return {
+    fields: {
+      project: { id: projectId },
+      issuetype: { name: issueType },
+      summary: title,
+      description: description,
+      labels: [label]
+    },
+  };
+};
+
+const postTicket = async (ticketTitle, ticketDescription, label, issueType) => {
+  const body = getJiraTicketBody(ticketTitle, ticketDescription, label, issueType);
+  try {
+    const response = await authFetch(
+      "https://harryjira.atlassian.net/rest/api/2/issue",
+      {
+        body: JSON.stringify(body),
+        method: "POST",
+      }
+    );
+
+    const res = await response.json();
+    console.log(res.errors);
+  } catch (e) {
+    console.log("couldn't post jira ticket", e);
+  }
+};
+
+const constructLabelOptionArray = (labels) => {
+  return labels.map((label) => ({
+    text: { type: "plain_text", text: label },
+    value: label,
+  }));
+};
+
+const constructIssueTypeIdsArray = (issueTypeIds) => {
+  return issueTypeIds.map((issueTypeId) => ({
+    text: { type: "plain_text", text: issueTypeId },
+    value: issueTypeId,
+  }));
+};
+
+module.exports = {
+  getGPTSummary,
+  getMessagesFromThread,
+  getProjectLabels,
+  getProjectIssueTypes,
+  postTicket,
+  constructLabelOptionArray,
+  constructIssueTypeIdsArray,
+};
